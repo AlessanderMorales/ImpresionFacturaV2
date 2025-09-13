96a1c90a310f2d02cf555b51b94c79af07efdffc
Alessander Morales <alessmoralesmo@gmail.com>
2025-09-13 14:45:07 -0400

    Clase Tienda Mejorada

--- src/models/Tienda.js
+++ src/models/Tienda.js
@@ -1,9 +1,16 @@
-export default class Tienda {
-  constructor(nombre_tienda, ubicacion, telefono, codigo_autorizacion, nit) {
-    this.nombre_tienda = nombre_tienda;
-    this.ubicacion = ubicacion;
-    this.telefono = telefono;
-    this.codigo_autorizacion = codigo_autorizacion;
-    this.nit = nit;
-  }
+
+export class Tienda {
+    constructor(nombre_tienda, ubicacion, telefono, codigo_autorizacion, nit) {
+        if (!nombre_tienda || typeof nombre_tienda !== 'string') throw new Error("Nombre de tienda inválido.");
+        if (!ubicacion || typeof ubicacion !== 'string') throw new Error("Ubicación de tienda inválida.");
+        if (typeof telefono !== 'number' || telefono <= 0) throw new Error("Teléfono de tienda inválido.");
+        if (typeof codigo_autorizacion !== 'number' || codigo_autorizacion <= 0) throw new Error("Código de autorización inválido.");
+        if (typeof nit !== 'number' || nit <= 0) throw new Error("NIT de tienda inválido.");
+
+        this.nombre_tienda = nombre_tienda;
+        this.ubicacion = ubicacion;
+        this.telefono = telefono;
+        this.codigo_autorizacion = codigo_autorizacion;
+        this.nit = nit;
+    }
 }
