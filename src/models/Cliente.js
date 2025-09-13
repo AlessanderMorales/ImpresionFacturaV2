1551286f491a6fbbf46ce5e09d3029b8f8a522da
Alessander Morales <alessmoralesmo@gmail.com>
2025-09-13 14:34:08 -0400

    Clase Cliente con principios solid

--- src/models/Cliente.js
+++ src/models/Cliente.js
@@ -1,7 +1,11 @@
-export default class Cliente {
-  constructor(id, nombreYApellido, nit) {
-    this.id = id;
-    this.nombreYApellido = nombreYApellido;
-    this.nit = nit;
-  }
+export class Cliente {
+    constructor(id, nombreYApellido, nit) {
+        if (!id || typeof id !== 'string') throw new Error("ID de cliente inválido.");
+        if (!nombreYApellido || typeof nombreYApellido !== 'string') throw new Error("Nombre y Apellido de cliente inválido.");
+        if (typeof nit !== 'number' || nit < 0) throw new Error("NIT de cliente inválido.");
+
+        this.id = id;
+        this.nombreYApellido = nombreYApellido;
+        this.nit = nit;
+    }
 }
