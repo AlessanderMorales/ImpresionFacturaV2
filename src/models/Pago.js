5c1996d30fb990f1c48ea26aafcea4b5e395f1ad
Alessander Morales <alessmoralesmo@gmail.com>
2025-09-13 13:57:21 -0400

    Creando la clase Abstracta Pago

---
+++ src/models/Pago.js
@@ -0,0 +1,18 @@
+class Pago {
+    constructor(monto, montoEnLetras) {
+        if (new.target === Pago) {
+            throw new Error("La clase 'Pago' es abstracta y no puede ser instanciada directamente.");
+        }
+        if (typeof monto !== 'number' || monto < 0) {
+            throw new Error("El monto debe ser un número positivo.");
+        }
+        if (typeof montoEnLetras !== 'string' || montoEnLetras.trim() === '') {
+            throw new Error("El monto en letras no puede estar vacío.");
+        }
+        this.monto = monto;
+        this.montoEnLetras = montoEnLetras;
+    }
+    realizarPago() {
+        throw new Error("El método 'realizarPago()' debe ser implementado por las subclases.");
+    }
+}
