a6cddbb781dce836ff0847468412f78b5e4547cc
Alessander Morales <alessmoralesmo@gmail.com>
2025-09-13 13:39:54 -0400

    Creando la clase Tienda

---
+++ src/models/Tienda.js
@@ -0,0 +1,9 @@
+export default class Tienda {
+  constructor(nombre_tienda, ubicacion, telefono, codigo_autorizacion, nit) {
+    this.nombre_tienda = nombre_tienda;
+    this.ubicacion = ubicacion;
+    this.telefono = telefono;
+    this.codigo_autorizacion = codigo_autorizacion;
+    this.nit = nit;
+  }
+}
