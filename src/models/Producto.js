4c3a3c80fb624e751905c754dec3d31fb8bf2df0
Alessander Morales <alessmoralesmo@gmail.com>
2025-09-13 14:37:30 -0400

    Clase Producto con principios solid

--- src/models/Producto.js
+++ src/models/Producto.js
@@ -1,7 +1,11 @@
-export default class Producto {
-  constructor(id, nombre, precio) {
-    this.id = id;
-    this.nombre = nombre;
-    this.precio = precio;
-  }
+export class Producto {
+    constructor(id, nombre, precio) {
+        if (typeof id !== 'number' || id <= 0) throw new Error("ID de producto inválido.");
+        if (!nombre || typeof nombre !== 'string') throw new Error("Nombre de producto inválido.");
+        if (typeof precio !== 'number' || precio <= 0) throw new Error("Precio de producto inválido.");
+
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
 }
