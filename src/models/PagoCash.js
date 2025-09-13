9352ccfa91069ad36c4dbddbb5f6c202fca0ce6c
Alessander Morales <alessmoralesmo@gmail.com>
2025-09-13 13:59:29 -0400

    Creando la clase pago Cash

---
+++ src/models/PagoCash.js
@@ -0,0 +1,9 @@
+class PagoCash extends Pago {
+    constructor(monto, montoEnLetras) {
+        super(monto, montoEnLetras);
+    }
+    realizarPago() {
+        console.log(`Procesando pago en efectivo por ${this.monto} (${this.montoEnLetras}).`);
+        return `Pago en efectivo por ${this.monto} completado exitosamente.`;
+    }
+}
